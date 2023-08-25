4bf5bfbf4002382bceeb7c25d369b7e4f6e7483d
alexcoronacion <alex.coronacion2022@gmail.com>
2023-08-25 12:36:29 -0500

    250823

--- clasesObjetos.py
+++ clasesObjetos.py
@@ -1,8 +1,20 @@
-class Person:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+""" class Person:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+
 p1=Person("Christian Alexander",39)
-print(p1.name)
-print(p1.age)
+
+print(p1.nombre)
+print(p1.edad) """
+
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+    def __str__(self) -> str:
+        return f"{self.nombre}({self.edad})"
+
+p1=Persona("christian",39)
+print(p1)
 
